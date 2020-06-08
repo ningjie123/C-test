a69580d21b662c6164b01486de5e5d624872c38e
root <root@localhost.localdomain>
2020-06-08 20:59:37 +0800

    writetofasta.c

---
+++ writetofasta.c
@@ -0,0 +1,26 @@
+//write th sequence into fasta file
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void writetofasta(char *sequence)
+{
+  int i,j=0;
+  FILE *fp=NULL;
+  fp=fopen("*.fasta","w");
+  while(1){
+    i=0;
+    for(i;i<71;++i){
+      fputc(sequence[j],fp);
+      ++j;
+    }
+    fputc('\n',fp);
+    if (sequence[j]='\0') break;
+  }
+  fclose(fp);
+  
+  return 0;
+}
+  
+
