11f3f7a7a51bb184950c0b2cb252c0e621731d48
znn <2218467578@qq.com>
2020-06-09 22:00:35 +0800

    complement.c_1

--- complement.c
+++ complement.c
@@ -5,35 +5,37 @@
 #define MAX 100;
 
 //读文件过程中调用，遇到CDS后执行
-bool iscomplement(const char *s)
+int iscomplement(const char *s)
 {
     int i=0,j,k;
     char a[10] = "complement";
-    if (strlen(s) < 10) return false; //compare length
-    for ( i=0;((i+10<=strlen(s));i++ )
+    if (strlen(s) < 10) return 0; //compare length
+    for ( i=0;((i+10) <= strlen(s));i++ )
         if (s[i] == a[0])  //overlap
             {
                 j=i;
                 k=0;
                 while ((k < 10) && (s[j] == a[k])) {j++;k++;}
-                if (k == 10) {return true;}
+                if (k == 10) {return 1;}
             }
-    return false;
+    return 0;
 }
 
 
 //命令行参数file
 int **read(char *file[])
 {
-    int **b =(int **)malloc(MAX*sizeof(int *));
-    for (int i=0;i<MAX;i++)
+    int i;
+    int **b =(int **)malloc(10*sizeof(int *));
+    for (i=0;i<MAX;i++)
         b[i]=(int *)malloc(2*sizeof(int));
     FILE *ifp_a;
     char c;
     int newline=0;
-    int i=0,m=0,j=0;
+    int m=0,j=0;
     char a[100];
     if ((ifp_a = fopen(*file,"r")) == NULL) {printf("open file error!\n");exit(1);}
+    i=0;
 
     while ((c=getc(ifp_a)) != EOF)
     {
